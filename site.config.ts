3286824b0c87bbd0096fbb913c1839407c171f08
twone <twonehk2016@gmail.com>
2023-03-14 15:21:37 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -18,7 +18,7 @@ export default siteConfig({
   // social usernames (optional)
   twitter: 'tokyo10086',
   github: 'ohmyself',
-  YouTube: '#channel/UCDcD8uwHi7H-OkPow7iFgqQ',
+  YouTube: '#0xtwone',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
